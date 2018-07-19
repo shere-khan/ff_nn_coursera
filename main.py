15e00fc1d527009d61e6800a8a826d3bf6fbf039
justin <justin.barry.e@gmail.com>
2018-07-19 12:55:43 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+
+np.random.seed(1)
+
+def initialize_parameters(n_x, n_h, n_y):
+    np.random.seed(1)
+
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+
+    return parameters
+
+def initialize_parameters_deep(layer_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims)  # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l],
+                                                   layer_dims[l - 1]) * 0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+
+        assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+    return parameters
+
+def linear_forward(A, W, b):
+    Z = np.dot(W, A) + b
+
+    assert (Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
+
+    return Z, cache
+
+def linear_activation_forward(A_prev, W, b, activation):
+    """
+    Implement the forward propagation for the LINEAR->ACTIVATION layer
+
+    Arguments:
+    A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+
+    Returns:
+    A -- the output of the activation function, also called the post-activation value
+    cache -- a python dictionary containing "linear_cache" and "activation_cache";
+             stored for computing the backward pass efficiently
+    """
+
+    Z, linear_cache = linear_forward(A_prev, W, b)
+    if activation == "sigmoid":
+        A, activation_cache = sigmoid(Z)
+
+    elif activation == "relu":
+        A, activation_cache = relu(Z)
+
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
+
+if __name__ == '__main__':
+    params = initialize_parameters_deep([5, 4, 1])
+    print("W1 = " + str(params["W1"]))
+    print("b1 = " + str(params["b1"]))
+    print("W2 = " + str(params["W2"]))
+    print("b2 = " + str(params["b2"]), end='\n\n')
+
+    X = np.random.rand(5, 7)
+    print(X)
+
+    Z, linear_cache = linear_forward(X, params['W1'], params['b1'])
+    print("Z = " + str(Z))
