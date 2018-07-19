74b0589065ee3bc868a0f316968e3954b28a7e2a
justin <justin.barry.e@gmail.com>
2018-07-19 16:42:23 -0400

    impl untested L_model_backward

--- main.py
+++ main.py
@@ -152,11 +152,10 @@ def linear_backward(dZ, cache):
     A_prev, W, b = cache
     m = A_prev.shape[1]
 
-    ### START CODE HERE ### (≈ 3 lines of code)
+    # todo: check db
     dW = np.divide(np.matmul(dZ, np.matrix.transpose(A_prev)), m)
     db = np.sum(dZ) / m
     dA_prev = np.matmul(np.matrix.transpose(W), dZ)
-    ### END CODE HERE ###
 
     assert (dA_prev.shape == A_prev.shape)
     assert (dW.shape == W.shape)
@@ -165,10 +164,14 @@ def linear_backward(dZ, cache):
     return dA_prev, dW, db
 
 def sigmoid_backward(dA, activation_cache):
-    pass
+    g = sigmoid(activation_cache[1])
+    dg = 1 - g
+
+    return dA * g * dg
 
 def relu_backward(dA, activation_cache):
-    pass
+    # todo
+    return 0
 
 def linear_activation_backward(dA, cache, activation):
     """
@@ -186,17 +189,11 @@ def linear_activation_backward(dA, cache, activation):
     """
     linear_cache, activation_cache = cache
 
-    if activation == "relu":
-        ### START CODE HERE ### (≈ 2 lines of code)
-        dZ = None
-        dA_prev, dW, db = None
-        ### END CODE HERE ###
-
-    elif activation == "sigmoid":
-        ### START CODE HERE ### (≈ 2 lines of code)
-        dZ = None
-        dA_prev, dW, db = None
-        ### END CODE HERE ###
+    if activation == "sigmoid":
+        dZ = sigmoid_backward(dA, activation_cache)
+    else:
+        dZ = relu_backward(dA, activation_cache)
+    dA_prev, dW, db = linear_backward(dZ, linear_cache)
 
     return dA_prev, dW, db
 
@@ -218,32 +215,34 @@ def L_model_backward(AL, Y, caches):
              grads["db" + str(l)] = ...
     """
     grads = {}
-    L = len(caches)  # the number of layers
+    L = len(caches)
     # m = AL.shape[1]
     m = AL.shape[0]
-    Y = Y.reshape(AL.shape)  # after this line, Y is the same shape as AL
+    Y = Y.reshape(AL.shape)
 
     # Initializing the backpropagation
     dAL = -(np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
 
-    # Lth layer (SIGMOID -> LINEAR) gradients. Inputs: "dAL, current_cache". Outputs: "grads["dAL-1"], grads["dWL"], grads["dbL"]
-    ### START CODE HERE ### (approx. 2 lines)
-    current_cache = None
-    # grads["dA" + str(L - 1)], grads["dW" + str(L)], grads["db" + str(L)] = \
-    #     linear_activation_backwards
-    ### END CODE HERE ###
+    # Lth layer (SIGMOID -> LINEAR) gradients. Inputs: "dAL, current_cache".
+    # Outputs: "grads["dAL-1"], grads["dWL"], grads["dbL"]
+    current_cache = caches[L - 1]
+    grads["dA" + str(L - 1)], grads["dW" + str(L)], grads["db" + str(L)] = \
+        linear_activation_backward(dAL, current_cache, 'sigmoid')
 
     # Loop from l=L-2 to l=0
     for l in reversed(range(L - 1)):
         # lth layer: (RELU -> LINEAR) gradients.
-        # Inputs: "grads["dA" + str(l + 1)], current_cache". Outputs: "grads["dA" + str(l)] , grads["dW" + str(l + 1)] , grads["db" + str(l + 1)]
-        ### START CODE HERE ### (approx. 5 lines)
-        current_cache = None
-        dA_prev_temp, dW_temp, db_temp = None
-        grads["dA" + str(l)] = None
-        grads["dW" + str(l + 1)] = None
-        grads["db" + str(l + 1)] = None
-        ### END CODE HERE ###
+        # Inputs: "grads["dA" + str(l + 1)], current_cache".
+        # Outputs: "grads["dA" + str(l)] , grads["dW" + str(l + 1)],
+        # grads["db" + str(l + 1)]
+        # TODO: Check
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(
+            grads["dA" + str(l + 1)], current_cache, 'relu')
+
+        grads["dA" + str(l)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
 
     return grads
 
